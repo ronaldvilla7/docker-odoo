0c279314e0b9f0e915b0b4e04a30517dd7400908
Ronald Villa <ronaldvilla7@gmail.com>
2026-03-09 07:36:50 -0400

    feat(hr_contract_benefits): add base module structure for hr_contract_benefits

---
+++ src/custom/task_ronald_villa/hr_contract_benefits/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Gestión de Nómina y Beneficios (Custom)',
+    'version': '1.0',
+    'category': 'Human Resources/Payroll',
+    'summary': 'Generación de recibos de nómina y reglas de beneficios automáticos por tipo de contrato',
+    'description': """
+        Este módulo provee un ecosistema autónomo para generar recibos de nómina (Payslips)
+        en Odoo Community, inyectando el salario base del empleado e integrando reglas de beneficios
+        automáticamente en función del tipo de contrato (Tiempo Completo, Medio Tiempo, etc).
+        
+        No requiere dependencias externas como hr_payroll de Enterprise.
+    """,
+    'author': 'Ronald Villa',
+    'depends': ['hr', 'hr_contract'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_contract_benefit_rule_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_payslip_views.xml',
+        'views/hr_payslip_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
