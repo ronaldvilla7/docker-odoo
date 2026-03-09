cbdb166f4ce4a01d529c3d9b6c66755677553351
Ronald Villa <ronaldvilla7@gmail.com>
2026-03-09 08:01:01 -0400

    feature(account): add base module structure account_discount_rules

---
+++ src/custom/task_ronald_villa/account_discount_rules/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Políticas de Descuento en Facturas',
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'summary': 'Aplicación automática de descuentos en facturas según el tipo de cliente',
+    'description': """
+        Módulo para configurar reglas de descuento basadas en el tipo de cliente (Minorista, Mayorista, VIP).
+        Aplica los descuentos automáticamente al validar facturas o de forma manual.
+    """,
+    'author': 'Ronald Villa',
+    'depends': ['base', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_views.xml',
+        'views/account_discount_rule_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
